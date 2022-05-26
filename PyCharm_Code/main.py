6db5cf0e334504fddb2a5f670345b80ad85b1250
Joses <joses.chan@gmail.com>
2022-05-26 15:53:48 +0100

    Upload

--- PyCharm_Code/main.py
+++ PyCharm_Code/main.py
@@ -16,32 +16,28 @@
 from itertools import count
 from tkinter.ttk import LabeledScale
 from collections import deque
-import torch
-import torchvision
-import torchvision.transforms as transforms
+import keras
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import os
 import cv2
 import random
 import numpy as np
 import pandas
-import torch.utils.data
 import pydicom
 import sklearn
-from torch import nn
-from torch.utils.data import DataLoader
-from torch.optim import Adam
-from torchvision import datasets, transforms
-from torchvision.utils import make_grid
+import scipy
+import h5py
 from pydicom.pixel_data_handlers import apply_voi_lut
-from torchvision.models import vgg19_bn
-from scipy.ndimage import zoom
-from torch.autograd import Variable
+from pydicom.pixel_data_handlers.util import apply_modality_lut
+from scipy.ndimage import zoom, rotate
 from tqdm.auto import tqdm
 from sklearn.metrics import classification_report, roc_auc_score, roc_curve, confusion_matrix
 from sklearn.model_selection import train_test_split
-
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
+from tensorflow import keras
+from tensorflow.keras import layers
+from keras.models import load_model
+import time
 
 # file locations
 covid_files_path = 'C:\\Users\\Shadow\\Documents\\GitHub\\CNN_COVID_19\\PyCharm_Code\\CT_Scans\\Covid_Positive\\'
@@ -60,189 +56,183 @@ covidNegativeDf = pandas.DataFrame(nonCovidPatients)
 covidNegativeDf.insert(1, "label", 0, True)
 print(covidNegativeDf.head())
 
-patientDf = pandas.concat([covidPositiveDf.head(3),covidNegativeDf.head(3)])
+patientDf = pandas.concat([covidPositiveDf.sample(frac=0.20, replace=True, random_state=42),
+covidNegativeDf.sample(frac=0.20, replace=True, random_state=42)])
 patientDf = patientDf.astype({'label':'int'})
 print(patientDf.head(6))
 
-chosenDf = patientDf.head(6)
+chosenDf = patientDf
 chosenDf.insert(2, "prediction", None, allow_duplicates=True)
 chosenDf.insert(3, "dataID", None, allow_duplicates=False)
 chosenDf.insert(4, "data", None, allow_duplicates=False)
 print("Total Patient Count")
 print(len(chosenDf))
 
-       
+	   
 # Source Code for resizer and SIZ
-#https://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynbhttps://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynbhttps://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynbhttps://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynbhttps://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynbhttps://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynbhttps://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynbhttps://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynb
-
+# https://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynbhttps://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynbhttps://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynbhttps://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynbhttps://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynbhttps://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynbhttps://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynbhttps://github.com/hasibzunair/uniformizing-3D/blob/master/1_data_process_clef19.ipynb
+# Source Code for augmentation
+# https://towardsdatascience.com/simple-3d-mri-classification-ranked-bronze-on-kaggle-87edfdef018a
 # Resize 2D slices using bicubic interpolation to common CT size
 def rs_img(img):
-    img = cv2.resize(img, (256, 256), interpolation=cv2.INTER_CUBIC)
-    return img
+	img = cv2.resize(img, (128, 128), interpolation=cv2.INTER_CUBIC)
+	return img
 
-# Spline interpolated zoom (SIZ)
+def normalise(data):
+	max = 400
+	min = -1000
+	data[data > max] = max
+	data[data < min] = min
+	data = (data - min) / (max - min)
+	data = data.astype("float32")
+	return data
+
+# Spline interpolation
 def change_depth_siz(img):
-    desired_depth = 64
-    current_depth = img.shape[0]
-    print("Current Depth: ", current_depth)
-    depth = current_depth / desired_depth
-    depth_factor = 1 / depth
-    img_new = zoom(img, (depth_factor, 1, 1), mode='nearest')
-    print("New Depth: ", img_new.shape[0])
-    print(img_new.shape)
-    return img_new
+	desired_depth = 64
+	current_depth = img.shape[0]
+	print("Current Depth: ", current_depth)
+	depth = current_depth / desired_depth
+	depthFactor = 1 / depth
+	img_new = zoom(img, (depthFactor, 1, 1), mode='nearest')
+	print("New Depth: ", img_new.shape[0])
+	print(img_new.shape)
+	return img_new
+
+def resize_data(img):
+	# Set the desired depth
+	desired_depth = 64
+	desired_width = 128
+	desired_height = 128
+	# Get current depth
+	current_depth = img.shape[-1]
+	current_width = img.shape[0]
+	current_height = img.shape[1]
+	# Compute depth factor
+	depth = current_depth / desired_depth
+	width = current_width / desired_width
+	height = current_height / desired_height
+	depth_factor = 1 / depth
+	width_factor = 1 / width
+	height_factor = 1 / height
+	# Rotate
+	# img = scipy.ndimage.rotate(img, 90, reshape=False)
+	# Resize across z-axis
+	# img = change_depth_siz(img)
+	img = scipy.ndimage.zoom(img, (width_factor, height_factor, depth_factor), order=1)
+	return img
 
 yImages = []
 def loadProcessImage(dataset, processedCTScans):
-    for i in range(len(dataset)):
-        print("Index location: ", i)
-        dataset.iat[i, 3]
-        label = None
-        label = dataset.iat[i, 1]
-        imageSlice = []
-        path = ""
+	for i in range(len(dataset)):
+		print("Index location: ", i)
+		dataset.iat[i, 3]
+		label = None
+		label = dataset.iat[i, 1]
+		imageSlice = []
+		path = ""
 
-        if(label==0):
-            path = non_covid_files_path + dataset.iat[i,0]
+		if(label==0):
+			path = non_covid_files_path + dataset.iat[i,0]
 
-        if(label==1):
-            path = covid_files_path + dataset.iat[i,0]
+		if(label==1):
+			path = covid_files_path + dataset.iat[i,0]
 
-        print(path)
+		print(path)
 
-        # read in slices
-        slices = [pydicom.read_file(path+"/"+s) for s in os.listdir(path)]
-        # sort slices by their image position in comparison to other slices 
-        slices.sort(key=lambda x: int(x.ImagePositionPatient[2]))
-        print(len(slices), slices[0].Rows, slices[0].Columns)
-        
-        # collect CT scan data as a pixel array, resize then store
-        for imageCount in range(len(slices)):
-            imageSlice.append(rs_img(slices[imageCount].pixel_array))
-        print("Resize x and y complete")
+		# read in slices
+		slices = [pydicom.read_file(path+"/"+s) for s in os.listdir(path)]
+		# sort slices by their image position in comparison to other slices 
+		slices.sort(key=lambda x: int(x.ImagePositionPatient[2]))
+		print(len(slices), slices[0].Rows, slices[0].Columns)
+		
+		# collect CT scan data as a pixel array, resize then store
+		for imageCount in range(len(slices)):
+			imageNormalised = normalise(slices[imageCount].pixel_array)
+			imageVOI = apply_voi_lut(slices[imageCount].pixel_array, slices[imageCount]).astype(np.float32)
+			imageSlice.append((imageNormalised))
+		print("Resize x and y complete")
 
-        # Apply sampling technique
-        imageSlice = change_depth_siz(np.asanyarray(imageSlice))
-        imageSlice = torch.tensor(imageSlice)
-        print(imageSlice.size())
-        processedCTScans.append(imageSlice)
-        dataset.iat[i, 4] = imageSlice
-        dataset.iat[i, 3] = i
-        # plt.imshow(imageSlice[0])
-        # plt.show()
-        imageSlice=None
-    print(np.asanyarray(processedCTScans).shape)
+		# Apply sampling technique
+		imageSlice = resize_data(np.asanyarray(imageSlice))
+		# print(imageSlice.shape)
+		processedCTScans.append(imageSlice)
+		dataset.iat[i, 4] = imageSlice
+		dataset.iat[i, 3] = i
+		# plt.imshow(imageSlice[0])
+		# plt.show()
+		imageSlice=None
+	print(np.asanyarray(processedCTScans).shape)
  
-class cnnSimple(nn.Module):
-    def __init__(self):
-        super(cnnSimple, self).__init__()
-        
-        self.conv_layer1 = self._conv_layer_set(3, 32)
-        self.conv_layer2 = self._conv_layer_set(32, 64)
-        self.fc1 = nn.Linear(512, 128)
-        self.fc2 = nn.Linear(128, 2)
-        self.relu = nn.LeakyReLU()
-        self.batch=nn.BatchNorm1d(128)
-        self.drop=nn.Dropout(p=0.15)        
-        
-    def _conv_layer_set(self, in_c, out_c):
-        conv_layer = nn.Sequential(
-        nn.Conv3d(in_c, out_c, kernel_size=(3, 3, 3), padding=0),
-        nn.LeakyReLU(),
-        nn.MaxPool3d((2, 2, 2)),
-        )
-        return conv_layer
-    
-
-    def forward(self, x):
-        # Set 1
-        out = self.conv_layer1(x)
-        out = self.conv_layer2(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc1(out)
-        out = self.relu(out)
-        out = self.batch(out)
-        out = self.drop(out)
-        out = self.fc2(out)
-        return out
-
-class cnnNeuralNet(nn.Module):
-    def __init__(self):
-        super(cnnNeuralNet, self).__init__()
-
-        self.sequentialLayers = nn.Sequential(
-        # Convolution and Pooling Layer 1
-        nn.Conv3d(in_channels = 3, out_channels = 64,kernel_size = (3,3,3)),
-        nn.MaxPool3d(kernel_size=(2, 2, 2)),
-        nn.BatchNorm3d(),
-
-        # Convolution and Pooling Layer 2
-        nn.Conv3d(in_channels = 64, out_channels = 64, Kernel_size=(3, 3, 3)),
-        nn.MaxPool3d(kernel_size=(2, 2, 2)),
-        nn.BatchNorm3d(),
-
-        # Convolution and Pooling Layer 3
-        nn.Conv3d(in_channels = 64, out_channels = 64, Kernel_size=(3, 3, 3)),
-        nn.MaxPool3d(kernel_size=(2, 2, 2)),
-        nn.BatchNorm3d(),
-
-        # Convolution and Pooling Layer 4        
-        nn.Conv3d(in_channels = 64, out_channels = 128, Kernel_size=(3, 3, 3)),
-        nn.MaxPool3d(kernel_size=(2, 2, 2)),
-        nn.BatchNorm3d(),
-
-        # Convolution and Pooling Layer 5
-        nn.Conv3d(in_channels = 64, out_channels = 256, Kernel_size=(3, 3, 3)),
-        nn.MaxPool3d(kernel_size=(2, 2, 2)),
-        nn.BatchNorm3d(),
-        
-        #Flatten
-        nn.Flatten(),
-        #Full connected layer
-        nn.ReLU(),
-        nn.Dropout3d(0.4),
-        nn.ReLU(),
-        nn.Dropout3d(0.4)
-       ) 
-
-    # feed foward function
-    def forward(self, x):
-        out = self.Model(x)
-        return out
-
 loadProcessImage(chosenDf, yImages)
 
-batchSize = 64
-epochs = 60
 
-xTrainingDf, xTesting, yTrainingDf, yTesting  = train_test_split(chosenDf["data"], chosenDf["label"], train_size = 0.6, test_size=0.4,random_state=42)
-
-xTraining, xVal, yTraining, yVal  = train_test_split(xTrainingDf, yTrainingDf, train_size = 0.8, test_size=0.2,random_state=42)
+xTraining, xTesting, yTraining, yTesting  = train_test_split(chosenDf["data"], chosenDf["label"], train_size = 0.7, test_size=0.3,random_state=42)
 
 
-train_x = torch.stack(xTraining.tolist())
-val_x = torch.stack(xVal.tolist())
-test_x = torch.stack(xTesting.tolist())
-train_y = torch.as_tensor(yTraining.to_numpy())
-val_y = torch.as_tensor(yVal.to_numpy())
-test_y = torch.as_tensor(yTesting.to_numpy())
+train_x = np.asanyarray(xTraining.tolist())
+test_x = np.asanyarray(xTesting.tolist())
+train_y = np.asanyarray(yTraining.tolist())
+test_y = np.asanyarray(yTesting.tolist())
 
-print("Training Data:\n", train_x)
-print("Testing Data:\n", test_x)
+print("Training Data:\n", train_x.shape)
+print("Testing Data:\n", test_x.shape)
 print("Y Train:\n", train_y)
 print("Y Test:\n", test_y)
 
-train = torch.utils.data.TensorDataset(train_x,train_y)
-validation = torch.utils.data.TensorDataset(val_x,val_y)
-test = torch.utils.data.TensorDataset(test_x, test_y)
+# dataloaders for the model
+train_loader = tf.data.Dataset.from_tensor_slices((train_x, train_y))
+validation_loader = tf.data.Dataset.from_tensor_slices((test_x, test_y))
 
-train_loader = torch.utils.data.DataLoader(train, batch_size = batchSize, drop_last=False, shuffle = False)
-validation_loader = torch.utils.data.DataLoader(train, batch_size = batchSize, drop_last=False, shuffle = False)
-test_loader = torch.utils.data.DataLoader(test, batch_size = batchSize, drop_last=False, shuffle = False)
+batch_size = 2
 
-# calculate steps per epoch for training and validation set
-trainSteps = len(train_loader.dataset) // batchSize
-valSteps = len(validation_loader.dataset) // batchSize
+@tf.function
+def rotate(volume):
+	def scipy_rotate(volume):
+		# angles that will be chosen randomly to rotate CT images
+		angles = [-20, -10, -5, 5, 10, 20]
+		angle = random.choice(angles)
+		volume = scipy.ndimage.rotate(volume, angle, reshape=False)
+		volume[volume < 0] = 0
+		volume[volume > 1] = 1
+		return volume
+	augmented_volume = tf.numpy_function(scipy_rotate, [volume], tf.float32)
+	return augmented_volume
+
+# Add rotation dimension to training data
+def train_preprocessing(volume, label):
+	volume = rotate(volume)
+	volume = tf.expand_dims(volume, axis=3)
+	return volume, label
+
+# Add rotation dimension to testing data
+def validation_preprocessing(volume, label):
+	volume = tf.expand_dims(volume, axis=3)
+	return volume, label
+
+
+# Changes values during training.
+train_dataset = (
+	train_loader.shuffle(len(train_x))
+	.map(train_preprocessing)
+	.batch(batch_size)
+	.prefetch(2)
+)
+
+# rescale
+validation_dataset = (
+	validation_loader.shuffle(len(test_x))
+	.map(validation_preprocessing)
+	.batch(batch_size)
+	.prefetch(2)
+)
+
+data = train_dataset.take(1)
+images, labels = list(data)[0]
+images = images.numpy()
+image = images[0]
+print("Dimension of the CT scan is:", image.shape)
+plt.imshow(np.squeeze(image[:, :, 30]), cmap="gray")
 
 #-------------------------------------------------Training and Testing the model-------------------------------------------------#
 
@@ -250,53 +240,100 @@ valSteps = len(validation_loader.dataset) // batchSize
 # https://blog.paperspace.com/fighting-coronavirus-with-ai-building-covid-19-classifier/
 # https://towardsdatascience.com/pytorch-step-by-step-implementation-3d-convolution-neural-network-8bf38c70e8b3
 # https://docs.microsoft.com/en-us/windows/ai/windows-ml/tutorials/pytorch-train-model
-
-# Define a convolution neural network
-class Network(nn.Module):
-    def __init__(self):
-        super(Network, self).__init__()
-        
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=5, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(12)
-        self.conv2 = nn.Conv2d(in_channels=12, out_channels=12, kernel_size=5, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(12)
-        self.pool = nn.MaxPool2d(2,2)
-        self.conv4 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=5, stride=1, padding=1)
-        self.bn4 = nn.BatchNorm2d(24)
-        self.conv5 = nn.Conv2d(in_channels=24, out_channels=24, kernel_size=5, stride=1, padding=1)
-        self.bn5 = nn.BatchNorm2d(24)
-        self.fc1 = nn.Linear(24*10*10, 10)
-
-    def forward(self, input):
-        output = torch.nn.functional.relu(self.bn1(self.conv1(input)))      
-        output = torch.nn.functional.relu(self.bn2(self.conv2(output)))     
-        output = self.pool(output)                        
-        output = torch.nn.functional.relu(self.bn4(self.conv4(output)))     
-        output = torch.nn.functional.relu(self.bn5(self.conv5(output)))     
-        output = output.view(-1, 24*10*10)
-        output = self.fc1(output)
-
-        return output
+# https://keras.io/examples/vision/3D_image_classification/#loading-data-and-preprocessing
 
 
+def get_model(width=128, height=128, depth=64):
 
-class LeNet(Module):
-	def __init__(self, numChannels, classes):
-		# call the parent constructor
-		super(LeNet, self).__init__()
-		# initialize first set of CONV => RELU => POOL layers
-		self.conv1 = nn.Conv3d(in_channels=numChannels, out_channels=20,
-			kernel_size=(5, 5))
-		self.relu1 = ReLU()
-		self.maxpool1 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-		# initialize second set of CONV => RELU => POOL layers
-		self.conv2 = Conv2d(in_channels=20, out_channels=50,
-			kernel_size=(5, 5))
-		self.relu2 = ReLU()
-		self.maxpool2 = MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
-		# initialize first (and only) set of FC => RELU layers
-		self.fc1 = Linear(in_features=800, out_features=500)
-		self.relu3 = ReLU()
-		# initialize our softmax classifier
-		self.fc2 = Linear(in_features=500, out_features=classes)
-		self.logSoftmax = LogSoftmax(dim=1)
+	inputs = keras.Input((width, height, depth, 1))
+
+	x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(inputs)
+	x = layers.MaxPool3D(pool_size=2)(x)
+	x = layers.BatchNormalization()(x)
+
+	x = layers.Conv3D(filters=64, kernel_size=3, activation="relu")(x)
+	x = layers.MaxPool3D(pool_size=2)(x)
+	x = layers.BatchNormalization()(x)
+
+	x = layers.Conv3D(filters=128, kernel_size=3, activation="relu")(x)
+	x = layers.MaxPool3D(pool_size=2)(x)
+	x = layers.BatchNormalization()(x)
+
+	x = layers.Conv3D(filters=256, kernel_size=3, activation="relu")(x)
+	x = layers.MaxPool3D(pool_size=2)(x)
+	x = layers.BatchNormalization()(x)
+
+	x = layers.GlobalAveragePooling3D()(x)
+	x = layers.Dense(units=512, activation="relu")(x)
+	x = layers.Dropout(0.3)(x)
+
+	outputs = layers.Dense(units=1, activation="sigmoid")(x)
+
+	# Define the model.
+	model = keras.Model(inputs, outputs, name="3dcnn")
+	return model
+
+
+# Build model.
+model = get_model(width=128, height=128, depth=64)
+model.summary()
+
+
+# Compile model.
+initial_learning_rate = 0.0001
+lr_schedule = keras.optimizers.schedules.ExponentialDecay(
+	initial_learning_rate, decay_steps=100000, decay_rate=0.96, staircase=True
+)
+model.compile(
+	loss="binary_crossentropy",
+	optimizer=keras.optimizers.Adam(learning_rate=lr_schedule),
+	metrics=["acc"],
+)
+
+# Define callbacks.
+checkpoint_cb = keras.callbacks.ModelCheckpoint(
+	"3d_image_classification.h5", save_best_only=True
+)
+early_stopping_cb = keras.callbacks.EarlyStopping(monitor="val_acc", patience=15)
+
+# Train the model, doing validation at the end of each epoch
+epochs = 100
+model.fit(
+	train_dataset,
+	validation_data=validation_dataset,
+	epochs=epochs,
+	shuffle=True,
+	verbose=2,
+	callbacks=[checkpoint_cb, early_stopping_cb],
+)
+
+fig, ax = plt.subplots(1, 2, figsize=(20, 3))
+ax = ax.ravel()
+
+for i, metric in enumerate(["acc", "loss"]):
+	ax[i].plot(model.history.history[metric])
+	ax[i].plot(model.history.history["val_" + metric])
+	ax[i].set_title("Model {}".format(metric))
+	ax[i].set_xlabel("epochs")
+	ax[i].set_ylabel(metric)
+	ax[i].legend(["train", "val"])
+plt.show()
+	
+# Load best weights.
+model.load_weights("3d_image_classification.h5")
+prediction = model.predict(test_x)
+# scores = [1 - prediction[0], prediction[0]]
+
+predictionList=[]
+# class_names = ["normal", "abnormal"]
+for predIndex in range(len(prediction)):
+	if(prediction[predIndex]<0.5):
+		predictionList.append(0)
+	else:
+		predictionList.append(1)
+print(prediction)
+print(predictionList)
+
+print(classification_report(test_y, predictionList, target_names=["covidNegative","covidPositive"]))
+
+
